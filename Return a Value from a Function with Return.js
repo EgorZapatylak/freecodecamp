2d4df9a0ce191c74d13548b3660dd148f688e5d1
EgorZapatylak <zapatylak96a@gmail.com>
2022-02-23 16:00:34 +0300

    Add task solution

---
+++ Return a Value from a Function with Return.js
@@ -0,0 +1,9 @@
+//Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(num) {
+    return num * 5;
+}
+
+timesFive(5);
+timesFive(2);
+timesFive(0);
