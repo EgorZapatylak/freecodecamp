e99c47389d92183924a1425623f8c69942d9a4c6
EgorZapatylak <zapatylak96a@gmail.com>
2022-02-28 10:16:02 +0300

    Create task solution

---
+++ Returning Boolean Values from Functions.js
@@ -0,0 +1,22 @@
+//
+// Fix the function isLess to remove the if/else statements.
+//
+// function isLess(a, b) {
+//     // Only change code below this line
+//     if (a < b) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+//     // Only change code above this line
+// }
+
+
+function isLess(a, b) {
+    // Only change code below this line
+    console.log(a < b);
+    // Only change code above this line
+}
+
+isLess(10, 15);
+isLess(15, 10);
