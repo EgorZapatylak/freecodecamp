148e3a71e6d7020711981291b9ded55315c1c2a2
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-17 14:30:17 +0300

    Add task solution

---
+++ Replace Loops using Recursion.js
@@ -0,0 +1,13 @@
+//Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
+}
+
+console.log(sum([1], 0));
+console.log(sum([2, 3, 4], 1));
+console.log(sum([2, 3, 4, 5], 3));
