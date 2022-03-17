3fe5d3a19125090c3012e354d0f04a32a734b671
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-17 16:02:54 +0300

    Add task solution

---
+++ Generate Random Whole Numbers with JavaScript.js
@@ -0,0 +1,11 @@
+//Use this technique to generate and return a random whole number between 0 and 9.
+
+
+function randomWholeNum() {
+
+    // Only change code below this line
+
+    console.log(Math.floor(Math.random() * 10));
+}
+
+randomWholeNum()
