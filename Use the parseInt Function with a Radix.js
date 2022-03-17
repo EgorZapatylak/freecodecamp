9fc444dd4e2933f152b841e03ac4a65d038d398f
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-17 16:10:41 +0300

    Add task solution

---
+++ Use the parseInt Function with a Radix.js
@@ -0,0 +1,9 @@
+//Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+function convertToInteger(str) {
+
+    console.log(parseInt(str, 2));
+
+}
+
+convertToInteger("10011");
