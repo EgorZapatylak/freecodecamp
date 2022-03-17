1ca6356bbe55536639ac3e36b06f0bec635c6b32
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-17 16:00:41 +0300

    Add task solution

---
+++ Generate Random Fractions with JavaScript.js
@@ -0,0 +1,12 @@
+//Change randomFraction to return a random number instead of returning 0.
+
+function randomFraction() {
+
+    // Only change code below this line
+
+    console.log(Math.random());
+
+    // Only change code above this line
+}
+
+randomFraction()
