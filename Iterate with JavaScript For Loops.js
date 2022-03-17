f8838104c284beb401a5e39ee8fff35e773301d3
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-17 11:32:42 +0300

    Add task solution

---
+++ Iterate with JavaScript For Loops.js
@@ -0,0 +1,12 @@
+//Use a for loop to push the values 1 through 5 onto myArray.
+
+// Setup
+const myArray = [];
+
+for (let i = 1; i <= 5; i++){
+    myArray.push(i);
+}
+
+// Only change code below this line
+
+console.log(myArray)
