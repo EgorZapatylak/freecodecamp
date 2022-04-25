c2bfd2241747a55d4c575e4f46e961d37b529c3d
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-25 23:15:36 +0300

    Create task solution

---
+++ ES 6/Handle a Fulfilled Promise with then.js
@@ -0,0 +1,12 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+makeServerRequest.then(result => {
+    console.log(result);
+});
